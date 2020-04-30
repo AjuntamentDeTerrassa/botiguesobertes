d74637525a1160194cd79a73e69617d1536e23f3
Josep Jaume <josepjaume@gmail.com>
2020-04-30 18:53:49 +0200

    Add image

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -5,24 +5,28 @@
       class="fixed top-0 left-0 h-full w-full flex items-center justify-center z-50 overflow-auto"
       style="background-color: rgba(0, 0, 0, 0.2);"
     >
-      <div class="bg-white shadow-lg rounded-lg max-w-2xl m-4 overflow-hidden">
-        <div class="bg-red-600 mb-8">
+      <div
+        class="bg-white shadow-lg rounded-lg max-w-2xl m-4 overflow-hidden bg-cover bg-center"
+        :style="`background-image: url(${womanUrl})`"
+      >
+        <div class="bg-red-600 mb-8 rounded-b-md w-64 mx-auto">
           <Logo
-            class="w-56 h-24 mx-auto mb-4 rounded-lg text-white fill-current"
+            class="w-40 p-2 h-20 mx-auto mb-4 rounded-lg text-white fill-current"
           />
         </div>
         <div class="p-16 pt-0">
-          <div class="text-lg pt-2">
-            A través d'aquest portal posem a la teva disposició la informació
-            relativa de totes les botigues i establiments de la nostra ciutat
-            perquè sàpigues els serveis que t'ofereixen a data d'avui per
-            comprar-hi de forma fàcil i ràpida.
+          <div class="pl-32 sm:pl-40 lg:pl-48 pt-6">
+            <div class="text-lg pt-2">
+              A través d'aquest portal posem a la teva disposició la informació
+              relativa de totes les botigues i establiments de la nostra ciutat
+              perquè sàpigues els serveis que t'ofereixen a data d'avui per
+              comprar-hi de forma fàcil i ràpida.
+            </div>
+            <div class="text-lg py-4">
+              Perquè les botigues de Terrassa
+              <span class="font-bold">ens agraden</span>.
+            </div>
           </div>
-          <div class="text-lg py-4">
-            Perquè les botigues de Terrassa
-            <span class="font-bold">ens agraden</span>.
-          </div>
-
           <button
             class="mx-auto block rounded-lg text-lg bg-red-600 shadow-md px-4 py-2 mt-8 text-white flex"
             @click="dismiss()"
@@ -38,11 +42,15 @@
 <script>
 import { mapState } from 'vuex'
 import Logo from '~/assets/logo-full.svg'
+let woman = require('~/assets/woman.jpg')
 
 export default {
   components: { Logo },
   computed: {
     ...mapState(['welcomeSeen']),
+    womanUrl() {
+      return woman
+    },
   },
   methods: {
     dismiss() {
