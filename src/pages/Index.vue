de8b72f6647dcafc9aaaec815fcaf95d16083f9b
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2021-04-14 15:33:58 +0200

    Filter shops with coordinates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="h-screen bg-gray-100">
-    <div class="flex flex-col lg:flex-row-reverse text-gray-800 w-full h-full">
+    <div class="flex flex-col w-full h-full text-gray-800 lg:flex-row-reverse">
       <div class="block lg:hidden">
         <LogoBar />
       </div>
       <div
-        class="flex-shrink-0 relative flex-1 lg:flex-auto lg:flex-grow lg:h-auto z-20 lg:mb-0 lg:-ml-2"
+        class="relative z-20 flex-1 flex-shrink-0 lg:flex-auto lg:flex-grow lg:h-auto lg:mb-0 lg:-ml-2"
       >
         <ClientOnly>
           <Map v-if="loaded" />
         </ClientOnly>
       </div>
       <div
-        class="flex-shrink flex flex-1 flex-col lg:flex-auto lg:flex-grow-0 lg:w-1/3 xl:w-1/4 overflow-auto shadow-xl-reverse lg:shadow-2xl z-40 lg:rounded-tr-lg bg-white"
+        class="z-40 flex flex-col flex-1 flex-shrink overflow-auto bg-white lg:flex-auto lg:flex-grow-0 lg:w-1/3 xl:w-1/4 shadow-xl-reverse lg:shadow-2xl lg:rounded-tr-lg"
       >
         <div class="hidden lg:block">
           <LogoBar />
@@ -20,7 +20,7 @@
         <ShopList />
       </div>
       <div
-        class="lg:hidden relative flex-grow-0 bg-white shadow-md-reverse z-50 overflow-hidden"
+        class="relative z-50 flex-grow-0 overflow-hidden bg-white lg:hidden shadow-md-reverse"
       >
         <FooterLinks />
       </div>
@@ -78,7 +78,7 @@ export default {
     ...mapState(['loaded']),
   },
   async mounted() {
-    let shops = this.$page.allShop.edges.map((edge) => edge.node)
+    let shops = this.$page.allShop.edges.map((edge) => edge.node).filter(shop => shop.coordinates)
     let types = [...new Set(shops.map((shop) => shop.type))].map((type) => ({
       id: type,
       name: type,
